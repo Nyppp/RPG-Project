27e3772f2f9f8b93970c6d2632086f98e9ff4661
Nyppp <wlsdnrla216@naver.com>
2022-12-16 15:49:36 +0900

    프로젝트 셋업, 샌드박스 구성

---
+++ Assets/Scripts/Mover.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class Mover : MonoBehaviour
+{
+
+    [SerializeField] Transform target;
+    NavMeshAgent nav;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        nav = GetComponent<NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        nav.destination = target.position;
+    }
+}

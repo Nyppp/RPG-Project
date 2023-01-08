696bffb67e9a5a7f0b7b08f6e9e8c0e7db2ed1b1
Nyppp <wlsdnrla216@naver.com>
2023-01-08 20:49:57 +0900

    공격 애니메이션 적용, 트리거 설정

--- Assets/Scripts/Combat/Fighter.cs
+++ Assets/Scripts/Combat/Fighter.cs
@@ -11,11 +11,16 @@ namespace RPG.Combat
     public class Fighter : MonoBehaviour, IAction
     {
         [SerializeField] float weaponRange = 2f;
+        [SerializeField] float timeBetweenAttacks = 1f;
 
         Transform target;
+        float timeSinceLastAttack = 0;
 
         private void Update()
         {
+            timeSinceLastAttack += Time.deltaTime;
+
+            //플레이어 공격 -> 공격 범위 밖에 있다면, 사거리에 올 떄 까지 쫓아감
             if (target == null) return;
 
             if (!GetInRange())
@@ -25,14 +30,26 @@ namespace RPG.Combat
             else
             {
                 GetComponent<Mover>().Cancel();
+                AttackBehaviour();
             }
         }
 
+        private void AttackBehaviour()
+        {
+            if(timeSinceLastAttack > timeBetweenAttacks)
+            {
+                GetComponent<Animator>().SetTrigger("attack");
+                timeSinceLastAttack = 0;
+            }
+        }
+
+        //타겟과 거리 계산
         private bool GetInRange()
         {
             return Vector3.Distance(target.position, transform.position) <= weaponRange;
         }
 
+        //액션스케쥴러에 공격 명령을 수행중이라고 알린 다음, 공격 타겟 설정
         public void Attack(CombatTarget combatTarget)
         {
             GetComponent<ActionScheduler>().StartAction(this);
@@ -42,6 +59,12 @@ namespace RPG.Combat
         public void Cancel()
         {
             target = null;
+        }
+
+        //애니메이션 이벤트 처리
+        void Hit()
+        {
+            print("데미지 계산");
         }
     }
 }

2d6cd11b32e925612b45426827ab99e2f5aa9e68
Nyppp <wlsdnrla216@naver.com>
2023-01-17 17:37:21 +0900

    레벨 디자인, 카메라 회전 기능 추가

--- Assets/Scripts/Core/FollowCamera.cs
+++ Assets/Scripts/Core/FollowCamera.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
@@ -7,12 +8,26 @@ namespace RPG.Core
     public class FollowCamera : MonoBehaviour
     {
         [SerializeField] Transform target;
-
+        [SerializeField] float rotateSpeed = 1f;
 
         //플레이어 움직임 이후 후처리 카메라설정 (update -> lateupdate 라이프사이클)
         private void LateUpdate()
         {
             transform.position = target.position;
+
+            RotateCamera();
+        }
+
+        private void RotateCamera()
+        {
+            if(Input.GetKey(KeyCode.A))
+            {
+                transform.Rotate(0, rotateSpeed*Time.deltaTime, 0);
+            }
+            if (Input.GetKey(KeyCode.D))
+            {
+                transform.Rotate(0, -rotateSpeed*Time.deltaTime, 0);
+            }
         }
     }
 }

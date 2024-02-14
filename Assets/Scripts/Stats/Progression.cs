8a7b73434f5ec2c852a2601b66217525d697aac6
Nyppp <wlsdnrla216@naver.com>
2024-02-14 22:06:38 +0900

    스탯 불러오는 함수 처리개선
    딕셔너리 사용

--- Assets/Scripts/Stats/Progression.cs
+++ Assets/Scripts/Stats/Progression.cs
@@ -1,4 +1,6 @@
 using UnityEngine;
+using System.Collections.Generic;
+using System;
 
 namespace RPG.Stats
 {
@@ -7,24 +9,63 @@ namespace RPG.Stats
     {
         [SerializeField] ProgressionCharacterClass[] characterClasses = null;
 
+
+        //optimal code : dictinary 타입 저장으로 탐색 시간 감소
+        Dictionary<CharacterClass, Dictionary<Stat, float[]>> lookupTable = null;
+
         public float GetStat(Stat stat, CharacterClass characterClass, int level)
         {
-            //모든 캐릭터 클래스 탐색
-            foreach (ProgressionCharacterClass progressionClass in this.characterClasses)
+            Buildlookup();
+
+
+            float[] levels =  lookupTable[characterClass][stat];
+
+            if (levels.Length < level)
             {
-                if (progressionClass.characterClass != characterClass) continue;
-
-                //모든 스탯 탐색
-                foreach (ProgressionStat progressionStat in progressionClass.stats)
-                {
-                    if (progressionStat.stat != stat) continue;
-
-                    if (progressionStat.levels.Length < level) continue;
-
-                    return progressionStat.levels[level - 1];
-                }
+                return 0;
             }
-            return 0;
+
+            return levels[level - 1];
+            //Legacy code : 모든 탐색에 대해 시간이 오래 걸림
+            /*public float GetStat(Stat stat, CharacterClass characterClass, int level)
+            {
+                //모든 캐릭터 클래스 탐색
+                foreach (ProgressionCharacterClass progressionClass in this.characterClasses)
+                {
+                    if (progressionClass.characterClass != characterClass) continue;
+
+                    //모든 스탯 탐색
+                    foreach (ProgressionStat progressionStat in progressionClass.stats)
+                    {
+                        if (progressionStat.stat != stat) continue;
+
+                        if (progressionStat.levels.Length < level) continue;
+
+                        return progressionStat.levels[level - 1];
+                    }
+                }
+                return 0;
+            }*/
+        }
+
+        private void Buildlookup()
+        {
+            if (lookupTable != null) return;
+
+            lookupTable = new Dictionary<CharacterClass, Dictionary<Stat, float[]>>();
+
+            foreach(ProgressionCharacterClass progressionCharacterClass in characterClasses)
+            {
+                var statlookupTable = new Dictionary<Stat, float[]>();
+
+                foreach (ProgressionStat progressionStat in progressionCharacterClass.stats)
+                {
+                    statlookupTable[progressionStat.stat] = progressionStat.levels;
+                }
+
+
+                lookupTable[progressionCharacterClass.characterClass] = statlookupTable;
+            }
         }
 
         //클래스, 구조체의 직렬화 -> Serializable 사용, 내부 변수도 SerializeField 선언하거나 public으로 공개
